98e8345e5352626fceb626e94e14dee9a84c6a37
ODonnchadha <danieldonahue1231@hotmail.com>
2023-04-01 07:49:11 -0500

    INIT.

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# fluentvalidation-fundamentals
-Culled from a Pluralsight class entitled "FluentValidation Fundamentals" by Vladimir Khorikov.
+## FluentValidation Fundamentals by Vladimir Khorikov
+
+- This course will teach you how to use FluentValidation library to perform validation in ASP.NET Core applications. 
+- [FluentValidation](https://fluentvalidation.net) is a free and open-source library developed by Jeremy Skinner.
+
+> Welcome to the Validation in DDD course
+> =====================
+> This is the source code for my Pluralsight course about Validation in DDD.
+> The course is currently under development. 
+> To get notified when it's released, subscribe to my email list: https://enterprisecraftsmanship.com/subscribe
+> How to Get Started
+> --------------
+> No need to set up a database, just hit F5 and you are good to go!
+
+- OVERVIEW:
+  - Validation is surprisingly complex.
+    - The FluentValidation library.
+    - Always-valid domain model.
+    - The Value Object design pattern.
+    - Explicit application errors.
+
+- INTRODUCTION:
+  - 
+  
+- VALIDATING INPUT WITH FLUENTVALIDATION:
+- DIVING DEEPER INTO FLUENTVALIDATION:
+- VALIDATING INPUT THE DDD WAY:
+- COMBINING FLUENTVALIDATION WITH DDD PATTERNS:
+- DIVING DEEPER INTO DDD AND VALIDATION:
---
+++ src/Api/StudentRepository.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DomainModel;
+
+namespace Api
+{
+    public class StudentRepository
+    {
+        private static readonly List<Student> existingStudents = new List<Student>
+        {
+            Alice(),
+            Bob()
+        };
+
+        private static long lastId = existingStudents.Max(x => x.Id);
+        public Student GetById(long id) => existingStudents.SingleOrDefault(x => x.Id == id);
+        public Student GetByEmail(Email email) => existingStudents.SingleOrDefault(x => x.Email == email);
+
+        public void Save(Student student)
+        {
+            if (student.Id == 0)
+            {
+                lastId++;
+                SetId(student, lastId);
+            }
+
+            existingStudents.RemoveAll(x => x.Id == student.Id);
+            existingStudents.Add(student);
+        }
+
+        private static void SetId(Entity entity, long id) =>
+            entity.GetType().GetProperty(nameof(Entity.Id)).SetValue(entity, id);
+
+
+        private static Student Alice()
+        {
+            var alice = new Student(
+                Email.Create("alice@gmail.com").Value,
+                "Alice Alison",
+                new[] { Address.Create("1234 Main St", "Arlington", "VA", "22201", new[] { "VA" }).Value });
+            SetId(alice, 1);
+            alice.Enroll(new[] { new Enrollment(new Course(1, "Calculus", 5), Grade.A) });
+
+            return alice;
+        }
+
+        private static Student Bob()
+        {
+            var bob = new Student(
+                Email.Create("bob@gmail.com").Value,
+                "Bob Bobson",
+                new[] { Address.Create("2345 Second St", "Barlington", "VA", "22202", new[] { "VA" }).Value });
+            SetId(bob, 2);
+            bob.Enroll(new[] { new Enrollment(new Course(2, "History", 4), Grade.B) });
+            
+            return bob;
+        }
+    }
+}
